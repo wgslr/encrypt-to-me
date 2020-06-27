f31c6e1ae3c28e5ab958adfed1d7c1463bba3ef4
Wojciech Geisler <wojciech.geisler@gmail.com>
2020-06-27 09:05:41 +0200

    Create Encryptor component

---
+++ components/Encryptor.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import moduleCss from "./Encryptor.module.css";
+
+interface IProps {
+  pubkey: string;
+}
+
+const Encryptor: React.FC<IProps> = ({ pubkey }) => {
+  const [input, setInput] = React.useState("");
+  const [encrypted, setEncrypted] = React.useState<string>(undefined);
+  const [error, setError] = React.useState<string>(undefined);
+  React.useEffect(() => {
+    if (!input) {
+      setEncrypted("");
+    } else if (pubkey) {
+      console.log("encrypting");
+      encrypt(input, pubkey)
+        .then(setEncrypted)
+        .catch((e) => {
+          console.log(e);
+          setError(e);
+        });
+    }
+  }, [input, pubkey]);
+
+  return (
+    <section className={moduleCss.container}>
+      <textarea
+        className={moduleCss.textarea}
+        value={input}
+        onChange={(event) => setInput(event.target.value)}
+      ></textarea>
+      <textarea
+        className={`${moduleCss.textarea} ${(error && moduleCss.error) || ""}`}
+        readOnly
+        value={error ? error : encrypted}
+        // style={{ width: "60em", height: "20em" }}
+      ></textarea>
+    </section>
+  );
+};
+export default Encryptor;
+
+async function encrypt(text: string, pubkey: string): Promise<string> {
+  const rage = await import("@wgslr/encrypt-to-me-rage");
+  return rage.encrypt_to_pubkey(text, pubkey);
+}
--- pages/pubkey/[...pubkey].tsx
+++ pages/pubkey/[...pubkey].tsx
@@ -1,20 +1,8 @@
 import { GetServerSideProps, GetStaticProps, GetStaticPaths } from "next";
 import Head from "next/head";
 import React from "react";
-import Layout from "../../components/layout";
-
-// export const getStaticPaths: GetStaticPaths = async () => {
-//   return {
-//     paths: [],
-//     fallback: true,
-//   };
-// };
-
-// export const getStaticProps: GetStaticProps = async ({ params }) => {
-//   const { pubkey } = params;
-
-//   return { props: { pubkey: (pubkey as string[]).join("/") } };
-// };
+import Layout from "../../components/Layout";
+import Encryptor from "../../components/Encryptor";
 
 export const getServerSideProps = async ({ params }) => {
   const { pubkey } = params;
@@ -24,44 +12,13 @@ export const getServerSideProps = async ({ params }) => {
 };
 
 export default function Page({ pubkey }) {
-  // initial render has empty pubkey because of static generation
-
-  console.log("function", { pubkey });
-  const [input, setInput] = React.useState("");
-  const [encrypted, setEncrypted] = React.useState(undefined);
-  const [error, setError] = React.useState(undefined);
-  React.useEffect(() => {
-    if (pubkey) {
-      encrypt(input, pubkey)
-        .then(setEncrypted)
-        .catch((e) => {
-          console.log(e);
-          setError(e);
-        });
-    }
-  }, [input, pubkey]);
   return (
     <Layout>
       {/* <Head>
         <title>{pubkey}</title>
       </Head> */}
       <div>Encrypting to {pubkey}</div>
-      <textarea
-        value={input}
-        onChange={(event) => setInput(event.target.value)}
-        // style={{ width: "60em", height: "20em" }}
-      ></textarea>
-      <textarea
-        disabled
-        value={encrypted}
-        // style={{ width: "60em", height: "20em" }}
-      ></textarea>
-      {(error && <div>{error.toString()}</div>) || ""}
+      <Encryptor pubkey={pubkey} />
     </Layout>
   );
-}
-
-async function encrypt(text: string, pubkey: string): Promise<string> {
-  const rage = await import("@wgslr/encrypt-to-me-rage");
-  return rage.encrypt_to_pubkey(text, pubkey);
 }
